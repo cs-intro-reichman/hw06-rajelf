773ddd37f7638e8eabe41fd9e68669003c098e79
Rajel Fishman <rajel@Rajels-MacBook-Pro.local>
2024-02-01 20:15:40 +0200

    7

--- Editor4.java
+++ Editor4.java
@@ -20,6 +20,22 @@ public class Editor4 {
           // Morphing from the source image to its grayscale version
           Runigram.morph(sourceImage, grayscaleImage, n);
       
+          import java.awt.Color;
+
+// Converts an Image to a greyscaled version of itself.
+// Takes in an image file name and the number of steps for morphing.
+
+public class Editor4 {
+
+	public static void main (String[] args) {
+		String source = args[0];
+		int n = Integer.parseInt(args[1]);
+		Color[][] sourceImage = Runigram.read(source);
+		Color[][] targetImage = Runigram.grayScaled(sourceImage);
+		Runigram.setCanvas(sourceImage);
+		Runigram.morph(targetImage, sourceImage, n);
+	}
+}
   
     
   }
